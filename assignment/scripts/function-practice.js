b6b9c3197d4422d3a39b8a263d907b79a72390c1
Isaac-Amano <yishakamano2020@gmail.com>
2024-07-16 01:42:00 -0500

    fixed curly bracket syntax error

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -75,8 +75,10 @@ console.log(getLast(['a', 'b', 'c']));
 console.log(getLast([]));     
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
-//    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find(value, array) {
+
+function find(value, array){
+
+
   for (let i = 0; i < array.length; i++) {
     if (array[i] === value) {
       return true;
